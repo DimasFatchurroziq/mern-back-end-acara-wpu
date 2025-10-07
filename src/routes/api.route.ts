d9db2e32d86d71f87427bbe72c946b48ad3c38f7
DimasFatchurroziq <dimasfatchurroziq.com>
2025-10-07 16:22:49 +0700

    plus: acl route

--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -1,8 +1,11 @@
+import { Request, Response, NextFunction } from "express";
 import express from "express";
 import { authController } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validate.middleware.js";
 import { activateUserSchema, createUserSchema, loginUserSchema } from "../validations/user.schema.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
+import aclMiddleware from "../middlewares/acl.middleware.js";
+import { ROLES } from "../utils/constant.js";
 
 const router = express.Router();
 
@@ -13,5 +16,15 @@ router.post('/auth/login', validate({body: loginUserSchema}), authController.log
 router.get('/auth/me', authMiddleware, authController.me);
 
 router.get('/auth/activation', validate({query: activateUserSchema}), authController.activation);
+
+router.get('/test-acl', 
+    [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])],
+        (req: Request, res: Response, next: NextFunction) => {
+            res.status(200).json({
+                message: "OK",
+                data: "succeess",
+            });
+        }
+    );
 
 export default router;
