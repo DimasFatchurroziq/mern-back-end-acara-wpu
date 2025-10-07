4181b17528b0c2fcdd955dbc10a07eae38984518
DimasFatchurroziq <dimasfatchurroziq.com>
2025-10-07 16:21:46 +0700

    feat: acl middleware

---
+++ src/middlewares/acl.middleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import { IReqUser } from "./auth.middleware.js";
+
+export default (roles: string[]) => {
+    return (req: IReqUser, res: Response, next: NextFunction): void => {
+        const role = req.user?.role;
+
+        if (!role) {
+            res.status(401).json({
+                    message: "Unauthorized: Missing authentication credentials.",
+                    data: null,
+                });
+            return;
+        };
+
+        if (!roles.includes(role)) {
+            res.status(403).json({
+                    message: "Forbidden: Access denied due to insufficient role.",
+                    data: null,
+                });
+            return;
+        };
+
+        next();
+
+    };
+};
